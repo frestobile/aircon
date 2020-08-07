79effdc6c0b19959a701d73f4d1ce2e612f393c4
Mohammad Arsil <62394264+arsil313@users.noreply.github.com>
2020-08-07 07:37:13 -0700

    Add files via upload

---
+++ js/internal.js
@@ -0,0 +1,13 @@
+
+    jQuery(document).ready(function($){		
+        $("#mobile-menu").mobileMenu({
+            MenuWidth: 250,
+            SlideSpeed : 300,
+            WindowsMaxWidth : 767,
+            PagePush : true,
+            FromLeft : true,
+            Overlay : true,
+            CollapseMenu : true,
+            ClassName : "mobile-menu"
+        });
+    });        
