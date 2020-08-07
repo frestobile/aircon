3487e2aac2e864cc83be42029e1d73e1c8a46606
Mohammad Arsil <62394264+arsil313@users.noreply.github.com>
2020-08-07 07:40:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,303 +1,136 @@
 
 <!DOCTYPE html>
-<!-- saved from url=(0024)http://airconranger.com/ -->
-<html  lang="ja">
+<html lang="en">
 <head>
-	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>エアコンのトラブル、即日駆けつけ・即日修理・出張無料　エアコンレンジャー</title>
-  <meta name="description" content="エアコンのトラブル、即日駆けつけ・即日修理・出張無料　エアコンレンジャー">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyBusiness</title>
+        <!-- online / offline google cdn -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-
-  <link rel="manifest" href="http://airconranger.com/site.webmanifest">
-  <link rel="apple-touch-icon" href="http://airconranger.com/img/icon.png">
-  <link rel="shortcut icon" href="http://airconranger.com/img/favicon.ico">
-
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/main.css">
- 
+        <script>
+            window.jquery || "<script src='js/offline-jQuery.js'><\/script>"
+        </script>
+      
+       
+        <!-- CSS LINK -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="style/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="style/internal.css">
+        <link rel="stylesheet" type="text/css" href="style/responsive.css">
+        <link rel="stylesheet" type="text/css" href="style/animate.css">
+        <link rel="stylesheet" href="style/slider-pro.min.css"/>
+         <link rel="stylesheet" href="style/jquery.mobile-menu.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
+      
+     
+        <!-- SCRIPT LINK -->
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/wow.js"></script>
+        <script src="js/internal.js"></script>
+        <script src="js/jquery-1.11.0.min.js"></script>
+        <script src="js/jquery.sliderPro.min.js"></script>
+         <script src="js/jquery.mobile-menu.js"></script>
 
 </head>
-
 <body>
-  <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
-  <!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5STNV9S"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->
-  <header class="header" role="banner">
-    <div class="wrapper flex-row">
-      <h1 class="logo"><figure><img class="logo-img" src="images/logoText.svg" alt="エアコンレンジャー"></figure></h1>
-      <div class="flex-row">
-        <a href="tel:050-5307-5642">
-          <div class="telBtn"><span class="s14">相談無料・年中無休</span><span class="s21">050-5307-5642</span></div>
-        </a>
-        <a href="#contact">
-          <div class="webBtn">WEBで申込み</div>
-        </a>
-      </div>
+    <div class="container-fluid bg-primary p-3" >
+        <span class="text-light font-weight-bold ">エアコン修理の</span> <br />
+        <span class="text-warning font-weight-bold" style="font-size: 30px;">スフィリオ</span>
+
+                    <div class="right-div font-weight-bold animated fadeIn infinite">東京/埼玉<br />
+            全域対応！！！</div>
     </div>
-  </header>
-  <main role="main">
-    <div id="mainVisual">
-      <img src="images/main.jpg" alt="エアコンの3大トラブルに即日対応　水漏れ　冷えない　その他トラブル">
+   <div id="navbar">
+    <div id="menu-nav">
+    
+      <a href="#">Phone number: 688-2384-3692</a>
+      <a href="#">Business hours: 8時~22時</a>
+      <a href="#">Motto/catch frase: 迅速な対応。他社より安い価格。</a>
+
+    
+       </div>
     </div>
-    <section id="price">
-      <article class="wrapper">
-        <div class="tLine"><div><p><img src="images/logoText.svg" class="textLogo" alt="エアコンレンジャー">なら</p><p>どこよりも早く・安く修理を行います！</p></div></div>
-        <div class="basePrice">基本料金<span>6,000</span>円〜</div>
-        <div class="flex-row">
-          <div class="boxA"><figure><img src="images/iconA.svg" alt="水漏れ"></figure><p>水漏れ</p></div>
-          <div class="boxA"><figure><img src="images/iconB.svg" alt="ガス充填"></figure><p>ガス充填</p></div>
-          <div class="boxA"><figure><img src="images/iconC.svg" alt="その他故障"></figure><p>その他故障</p></div>
-        </div>
-      </article>
-      <div class="charactor"><img src="images/cB.svg" alt="エアブルー"></div>
-    </section>
-    <section id="process">
-      <article class="wrapper">
-        <p class="tLine-w">修理までの流れ</p>
-        <div class="wrapper-s flex-row">
-          <div class="boxB">
-            <p class="title">STEP <span>1</span></p>
-            <p class="processBText">お電話・またはインターネットからご連絡ください。</p>
-          </div>
-          <figure class="arrow">
-            <img src="images/arrow.svg" alt="&gt;">
-          </figure>
-          <div class="boxB">
-            <p class="title">STEP <span>2</span></p>
-            <p class="processBText">即日現地にお伺いします。現場を確認しお見積りをお出しいたします。</p>
-          </div>
-          <figure class="arrow">
-            <img src="images/arrow.svg" alt="&gt;">
-          </figure>
-          <div class="boxB">
-            <p class="title">STEP <span>3</span></p>
-            <p class="processBText">お見積り・作業内容にご納得いただいてから修理を開始いたします。</p>
-          </div>
-        </div>
-        <div class="wrapper-s">
-          <p class="processText">エアコンが冷えない・エアコンから水漏れ・壊れたなど緊急修理、見積依頼、無料相談などお気軽にご連絡ください。</p>
-        </div>
-      </article>
-      <div class="charactor"><img src="images/cY.svg" alt="エアイエロー"></div>
-    </section>
-    <section id="feature">
-      <article class="wrapper">
-        <p class="tLine"><img src="images/logoText.svg" class="textLogo" alt="エアコンレンジャー">の特徴</p>
-        <div class="flex-row">
-          <div class="boxA">
-            <figure class="icon"><img src="images/cY.svg" alt="エアイエロー"></figure>
-            <p class="featureTitle"><span class="s30">地域密着</span><span class="s23">だからこそ</span><span class="s30">出来る即日対応</span></p>
-            <p class="featureText">エアコンレンジャーは各エリアにいる職人さんと業務提携を結び、お客様のお住まいのエリアの方がすぐに駆け付けられる体制を取っています。</p>
-          </div>
-          <div class="boxA">
-            <figure class="icon"><img src="images/cB.svg" alt="エアブルー"></figure>
-            <p class="featureTitle"><span class="s30">プロの技術養成</span></p>
-            <p class="featureText">エアコンレンジャーは社内に対する教育体制をしっかりと整えているため技術のムラがなく、安定したサービスを提供出来ます。</p>
-          </div>
-          <div class="boxA">
-            <figure class="icon"><img src="images/cG.svg" alt="エアグリーン"></figure>
-            <p class="featureTitle"><span class="s30">専門家</span><span class="s23">だからこそ</span><span class="s30">出来る低価格</span></p>
-            <p class="featureText">我々はエアコン修理専門の企業であるため、材料や仕入れなどを最も安く仕入れることが出来ます。</p>
-          </div>
-        </div>
-      </article>
-    </section>
-    <section id="voice">
-      <article class="wrapper">
-        <p class="tLine-w">お客様の声</p>
-        <div class="voiceWrapper wrapper-s">
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>急なご連絡にも即日対応して頂けました。また何かありましたら宜しく御願い致します。</p>
-            </div>
-          </div>
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>電気を切り替えただけで電気代も安くなりキャッシュバックもあり大変助かりました。ありがとうございます！</p>
-            </div>
-          </div>
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>電話の対応や職人さんが親切にご説明してくだり安心して作業を頼めました。</p>
-            </div>
-          </div>
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>とにかく早く、安く、夏場の暑さにはとてもありがたかったです！</p>
-            </div>
-          </div>
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>コロナウイルス対策もしっかりして来てくださったので安心して作業をお願い出来ました！</p>
-            </div>
-          </div>
-          <div class="voiceRow">
-            <div class="voiceContainer">
-              <div><img src="images/user.svg" alt="user"></div>
-              <p>電気の切り替えや作業とにかくスムーズでとても助かりました。<br>ありがとうございました！</p>
-            </div>
-          </div>
-        </div>
-      </article>
-      <div class="charactor"><img src="images/cG.svg" alt="エアグリーン"></div>
-    </section>
-    <section id="contact">
-      <article class="wrapper">
-        <p class="tLine">まずはお気軽にお問合せください</p>
-        <div class="centering">
-          <a href="tel:050-5307-5642">
-            <div class="telBtn"><span class="s14">相談無料・年中無休</span><span class="s28">050-5307-5642</span></div>
-          </a>
-        </div>
-        <form id="form" class="h-adr wrapper-s" method="post" action="mail.php" enctype=”multipart/form-data”>
-          <span class="p-country-name" style="display:none;">Japan</span>
-          <table id="formTable">
-            <tbody><tr>
-              <th>お名前<span class="required">必須</span></th>
-              <td><span class="inuptTitle">姓</span><input type="text" id="family-name" class="form-control" name="苗字" placeholder="山田" required="">
-                <span class="inuptTitle">　名</span><input type="text" id="first-name" class="form-control" name="名前" placeholder="花子" required=""></td>
-            </tr>
-            <tr>
-              <th>訪問先<span class="required">必須</span></th>
-              <td>
-                <table class="innerTable">
-                  <tbody><tr>
-                    <th>郵便番号</th>
-                    <td><input type="text" class="p-postal-code" name="zip1" size="3" maxlength="3" required=""> - <input type="text" class="p-postal-code" name="zip2" size="4" maxlength="4" required=""></td>
-                  </tr>
-                  <tr>
-                    <th>都道府県</th>
-                    <td><input type="text" class="p-region" name="都道府県" size="4" required=""></td>
-                  </tr>
-                  <tr>
-                    <th>市区町村</th>
-                    <td class="td100"><input type="text" class="p-locality" name="市区町村" required=""></td>
-                  </tr>
-                  <tr>
-                    <th>それ以降の住所・マンション名・<wbr>部屋番号</th>
-                    <td class="td100"><input type="text" class="p-street-address p-extended-address" name="それ以降"></td>
-                  </tr>
-                </tbody></table>
-              </td>
-            </tr>
-            <tr>
-              <th>電話番号<span class="required">必須</span></th>
-              <td class="td100"><input type="tel" class="form-control" name="電話番号" placeholder="00-0000-0000" required=""></td>
-            </tr>
-            <tr>
-              <th>メールアドレス<span class="required">必須</span></th>
-              <td class="td100"><input type="email" class="form-control" name="メールアドレス" placeholder="aircon@example.com" required=""></td>
-            </tr>
-            <tr>
-              <th>訪問日時<span class="required">必須</span></th>
-              <td>
-                <table class="innerTable">
-                  <tbody><tr>
-                    <th>第1希望</th>
-                    <td><input type="date" class="form-control" name="第1希望-日付" required=""></td>
-                    <td><select name="第1希望-時間" required="">
-                      <option disabled="" selected="" value="">時間を選択してください</option>
-                      <option value="08:00〜10:00">08:00〜10:00</option>
-                      <option value="10:00〜12:00">10:00〜12:00</option>
-                      <option value="12:00〜14:00">12:00〜14:00</option>
-                      <option value="14:00〜16:00">14:00〜16:00</option>
-                      <option value="16:00〜18:00">16:00〜18:00</option>
-                      <option value="18:00〜20:00">18:00〜20:00</option>
-                      <option value="20:00〜22:00">20:00〜22:00</option>
-                    </select></td>
-                  </tr>
-                  <tr>
-                    <th>第2希望</th>
-                    <td><input type="date" class="form-control" name="第2希望-日付" required=""></td>
-                    <td><select name="第2希望-時間" required="">
-                      <option disabled="" selected="" value="">時間を選択してください</option>
-                      <option value="08:00〜10:00">08:00〜10:00</option>
-                      <option value="10:00〜12:00">10:00〜12:00</option>
-                      <option value="12:00〜14:00">12:00〜14:00</option>
-                      <option value="14:00〜16:00">14:00〜16:00</option>
-                      <option value="16:00〜18:00">16:00〜18:00</option>
-                      <option value="18:00〜20:00">18:00〜20:00</option>
-                      <option value="20:00〜22:00">20:00〜22:00</option>
-                    </select></td>
-                  </tr>
-                  <tr>
-                    <th>第3希望</th>
-                    <td><input type="date" class="form-control" name="第3希望-日付" required=""></td>
-                    <td><select name="第3希望-時間" required="">
-                      <option disabled="" selected="" value="">時間を選択してください</option>
-                      <option value="08:00〜10:00">08:00〜10:00</option>
-                      <option value="10:00〜12:00">10:00〜12:00</option>
-                      <option value="12:00〜14:00">12:00〜14:00</option>
-                      <option value="14:00〜16:00">14:00〜16:00</option>
-                      <option value="16:00〜18:00">16:00〜18:00</option>
-                      <option value="18:00〜20:00">18:00〜20:00</option>
-                      <option value="20:00〜22:00">20:00〜22:00</option>
-                    </select></td>
-                  </tr>
-                </tbody></table>
-              </td>
-            </tr>
-            <tr>
-              <th>症状<span class="required">必須</span></th>
-              <td>
-                <input class="chk" type="checkbox" name="症状[]" value="水漏れ"> 水漏れ　<wbr>
-                <input class="chk" type="checkbox" name="症状[]" value="ガス充填"> ガス充填　<wbr>
-                <input class="chk" type="checkbox" name="症状[]" value="取付け"> 取付け　<wbr>
-                <input class="chk" type="checkbox" name="症状[]" value="取外し"> 取外し　<wbr>
-                <input class="chk" type="checkbox" name="症状[]" value="その他"> その他
-              </td>
-            </tr>
-            <tr><th></th><td>どのような症状ですか？<br><textarea name="症状の詳細" placeholder="こちらにお問い合わせ内容を入力してください。"></textarea></td></tr>
-            <tr>
-              <th>エコ得でんきに関して<span class="required">必須</span></th>
-              <td>
-                <input class="chk2" type="checkbox" name="エコ得でんきに関して[]" value="興味がある"> 興味がある　<wbr>
-                <input class="chk2" type="checkbox" name="エコ得でんきに関して[]" value="興味がない"> 興味がない　<wbr>
-                <input class="chk2" type="checkbox" name="エコ得でんきに関して[]" value="どちらでもない"> どちらでもない<br><span class="s13">（エコ電気を同時にお申し込みいただくことで、<wbr>最大10,000円の還元キャンペーンを実施中）</span>
-              </td>
-            </tr>
-          </tbody></table>
-          <button type="submit" class="btn btn-primary" disabled="">送信</button>
-        </form>
-      </article>
-    </section>
-  </main>
-  <footer class="footer index" role="contentinfo">
-    <div class="wrapper">
-      <figure><img src="images/logo.svg" alt="エアコンレンジャー"></figure>
-      <address>運営会社：株式会社ing<br>
-        住所：〒108-0071 東京都港区白金台5-4-3<br>
-        TEL: 050-5307-5642</address>
+       
+
+    <div class="mobile-menu">
+           <ul>
+            <li><a href="#"><i class="fa fa-phone text-warning"></i>&nbsp;Phone number: 688-2384-3692</a></li>
+            <li> <a href="#"><i class="fa fa-bank text-warning"></i>&nbsp;Business hours: 8時~22時</a></li>
+            <li> <a href="#"><i class="fa fa-angellist text-warning"></i>&nbsp;Motto/catch frase: 迅速な対応。他社より安い価格。</a></li>
+            <li><a href="#"><i class="fa fa-dollar text-warning"></i>&nbsp;Prices: 水漏れ14万円！ ガス充填2万円~2万5千円通常価格の6割以下！</a></li>
+            <li><a href="#"><i class="fa fa-book text-warning"></i>&nbsp;Campaign:基本出張作業費0¥ + お友達の紹介で5千円割引</a></li>
+        </ul>
     </div>
-  </footer>
-  <div id="pcSticker"><a href="#contact"><img src="images/sticky.svg" alt="今ならキャッシュバックキャンペーン"></a></div>
-  <a href="#contact"><div id="tabSticker"><img src="images/tabSticky.svg" alt="今ならキャッシュバックキャンペーン"></div></a>
-  <div id="spSticker"><a href="#contact"><img src="images/spSticky.svg" alt="今ならキャッシュバックキャンペーン"></a></div>
-  <script src="js/modernizr-3.6.0.min.js"></script>
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script>
-    window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')
-  </script>
-  <script src="js/plugins.js"></script>
-  <script src="js/main.js"></script>
-  <script src="js/bootstrap.min.js"></script>
 
 
-<div class="goog-te-spinner-pos"><div class="goog-te-spinner-animation"><svg xmlns="http://www.w3.org/2000/svg" class="goog-te-spinner" width="96px" height="96px" viewBox="0 0 66 66"><circle class="goog-te-spinner-path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
-</svg>
+
+
+<div class="container-fluid p-0 m-0" style="width: 99%">
+    
+<div class="row p-0">
+    <div class="col-md-4 p-0">       
+        <div class="slider-pro " id="my-slider">
+            <div class="sp-slides">
+                <!-- Slide 1 -->
+                <div class="sp-slide">
+                    <img class="sp-image" src="pics/image1.png"/>
+                </div>
+                
+                <!-- Slide 2 -->
+                <div class="sp-slide">
+                     <img class="sp-image" src="pics/image2.png"/>
+                </div>
+                
+                <!-- Slide 3 -->
+                <div class="sp-slide">
+                     <img class="sp-image" src="pics/image3.png"/>
+                </div>
+            </div>
+        </div>
+
+        <div class="down-side">
+            <h3 align="center"><b>電気の救助隊は安心価格！</h3><hr />
+                <h5>
+                    <span style="color: red">作業前に作業内容と見積もりを必ず提示します。
+                    お客様にご納得いただいたうえで作業に着手します。
+                    ご納得いただけなかった場合、費用は一切いただいていません。</b>
+                </span>
+                </h5>
+        </div>
+    </div>
+    <div class="col-md-8 p-0">
+        <img src="pics/image4.png" style="width: 95%;">
+    </div>
 </div>
 </div>
-</body></html>
+
+
+
+
+<script>
+    // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+$( document ).ready(function( $ ) {
+        $( '#my-slider' ).sliderPro();
+    });
+
+</script>
+</body>
+</html>
